953203a01a647d2374b04f25a04b3b0e2ae48f90
sam.loose <sloose@jaguar.csuchico.edu>
2018-09-26 23:04:03 -0700

    finished program

--- main.cpp
+++ main.cpp
@@ -8,18 +8,40 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
+  string item;
   bool quit = false;
+  bool quit2 = false;
 
-  do 
+  do
   {
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-  quit = input == 'q' || input == 'Q';
-  }
-  while(!quit);
-  return 0;
+      do 
+      {
+      cout<<"\n==GROCERY LIST MANAGER==";
+      cout<<"\nEnter your choice: ";
+      cout<<"\n (A)dd an item";
+      cout<<"\n (Q)uit";
+      cout<<"\nYour choice (A/Q): ";
+      cin>>input;
+      quit = input == 'q' || input == 'Q' || input == 'a' || input == 'A';
+      }
+      while(!quit);
+
+      if (input == 'a' || input == 'A')
+      {
+         if (numItems < 5)
+             {   
+                 cout<<"What is the item?"<<endl;
+                 cin>>item;
+                 list[numItems] = item;
+                 numItems++;
+             }
+         else
+             {
+                 cout<<"You'll need a bigger list!"<<endl;
+             }
+      }
+      quit2 = input == 'q' || input == 'Q';
+  }    
+  while(!quit2);
+return 0;
 }
