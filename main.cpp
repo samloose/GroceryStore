aa96f5178b798ff4adb41191581370280938b121
sam.loose <sloose@jaguar.csuchico.edu>
2018-09-26 20:30:47 -0700

    loop list

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author: Sam Loose
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,13 +8,18 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
+  bool quit = false;
 
+  do 
+  {
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
-
+  quit = input == 'q' || input == 'Q';
+  }
+  while(!quit);
   return 0;
 }
